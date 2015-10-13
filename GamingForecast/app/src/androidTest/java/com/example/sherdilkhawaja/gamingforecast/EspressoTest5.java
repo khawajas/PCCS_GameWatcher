fe58a71bfb08353168eba521814c7fa8f8c3742b
Rya <rustgip@vcu.edu>
2015-10-13 13:29:12 -0400

    The latest and greatest

--- GamingForecast/app/src/androidTest/java/com/example/sherdilkhawaja/gamingforecast/EspressoTest5.java
+++ GamingForecast/app/src/androidTest/java/com/example/sherdilkhawaja/gamingforecast/EspressoTest5.java
@@ -2,9 +2,13 @@ package com.example.sherdilkhawaja.gamingforecast;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.longClick;
+import static android.support.test.espresso.action.ViewActions.swipeDown;
 import static android.support.test.espresso.action.ViewActions.swipeLeft;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
 
 import android.app.ListActivity;
 import android.test.ActivityInstrumentationTestCase2;
@@ -24,12 +28,11 @@ public class EspressoTest5 extends ActivityInstrumentationTestCase2<MainActivity
 
     public void testClickAndCheckDisplayed() {
 
-    //code for favorites list
-        onView(withId(R.id.linearLayout)).perform(click());
-        assertNotNull(R.id.linearLayout);
-        //replace list_product with favorites list location.. cant find.
-
-
+        //onView(withId(R.string.no_favorites_items))
+          //      .check(matches(withText("HI!")));
+            onView(withId(R.id.list_product)).perform(click());
+            onView(withId(R.id.menu_favorites)).perform(click());
+            assertNotNull(R.id.menu_favorites);
     }
 }
 
