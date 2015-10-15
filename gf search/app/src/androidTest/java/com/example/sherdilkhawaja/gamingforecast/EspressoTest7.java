033eda5503fc6ff73e2a27391297c072c743fb28
khawajas <@vcu.edu>
2015-10-15 16:55:48 -0400

    This is what I have so far for the search function, my emulator doesn't work so i can't check to see if this works or not, therefore I made a separate folder for it. If it doesn't work will have to restart again...

---
+++ gf search/app/src/androidTest/java/com/example/sherdilkhawaja/gamingforecast/EspressoTest7.java
@@ -0,0 +1,45 @@
+package com.example.sherdilkhawaja.gamingforecast;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.view.View;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.longClick;
+import static android.support.test.espresso.action.ViewActions.swipeLeft;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.anyOf;
+import static org.hamcrest.Matchers.hasToString;
+import static org.hamcrest.core.AllOf.allOf;
+import static org.hamcrest.core.StringStartsWith.startsWith;
+
+import android.app.ListActivity;
+import android.test.ActivityInstrumentationTestCase2;
+
+import org.hamcrest.Matchers;
+
+//Scnenario: Video Sort [Happy Path] for Add Video Preview [IT 1]
+
+public class EspressoTest7 extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    public EspressoTest7() {
+        super(MainActivity.class);
+    }
+
+    public void setUp() throws Exception {
+        super.setUp();
+        getActivity();
+    }
+
+    public void testClickAndCheckDisplayed() {
+
+    }
+}
+
---
+++ gf search/app/src/main/java/com/example/sherdilkhawaja/gamingforecast/Product.java
@@ -0,0 +1,92 @@
+package com.example.sherdilkhawaja.gamingforecast;
+
+
+public class Product {
+
+    private int id;
+    private String game;
+    private String releaseDate;
+    private String platform;
+    private int rating;
+
+
+    public Product() {
+        super();
+    }
+
+    public Product(int id, String game, int rating, String releaseDate, String platform) {
+        super();
+        this.id = id;
+        this.game = game;
+        this.rating = rating;
+        this.rating = rating;
+        this.releaseDate = releaseDate;
+        this.platform = platform;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getGame() {
+        return game;
+    }
+
+    public void setName(String name) {
+        this.game = game;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public int getRating() {return rating; }
+
+    public void setRating(int rating) { this.rating = rating; }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Product other = (Product) obj;
+        if (id != other.id)
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Game: " + game + "\n" + "Release Date: "
+                + releaseDate + "\n" + "Platform: " + platform + "";
+    }
+}
